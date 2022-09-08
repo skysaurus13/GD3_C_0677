56f01fe630a51cd7b7a5e10e5ee322a3eea5d90b
skysaurus13 <lukito2717@gmail.com>
2022-09-08 13:55:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd3_c_0677/entity/Dosen.kt
@@ -0,0 +1,12 @@
+package com.example.gd3_c_0677.entity
+
+class Dosen(var name: String, var pengajar: String) {
+
+    companion object{
+        @JvmField
+        var listOfDosen = arrayOf(
+            Dosen("Fedelis Brian", "Pengajar Kelas A, B, dan D"),
+            Dosen("Thomas Adi", "Pengajar Kelas C")
+        )
+    }
+}
